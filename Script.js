773abf58046a20a2ca46ecfffb91961cfe9db6b9
lg11111111 <94802700+lg11111111@users.noreply.github.com>
2024-08-17 23:32:49 -0300

    Delete Script.js

--- Script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('clickMe').addEventListener('click', function() {
-    alert('Você clicou no botão!');
-});git init
-git add index.html style.css script.js
-git commit -m "Estrutura inicial do site sem pastas"
-git remote add origin https://github.com/lg11111111/aaaaaaaaaaa/tree/main
-git branch -M main
-git push -u origin main
