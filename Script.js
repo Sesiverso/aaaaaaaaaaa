b4de7ade7e253dbbc626e57b6f273bdbdf039f3d
lg11111111 <94802700+lg11111111@users.noreply.github.com>
2024-08-17 23:29:25 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,8 @@
+document.getElementById('clickMe').addEventListener('click', function() {
+    alert('Você clicou no botão!');
+});git init
+git add index.html style.css script.js
+git commit -m "Estrutura inicial do site sem pastas"
+git remote add origin 
+git branch -M main
+git push -u origin main
